e7139d5147a2b6cfd9ea88ce51c7efa73d7e70d0
Gabriel Llamas <gabriel_llamas_llopis@yahoo.es>
2013-09-04 13:55:33 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+argp
+====
+
+_Node.js project_
+
+#### Command line options parser ####
+
+Version: 0.0.0
+
+#### Installation ####
+
+```
+npm install argp
+```
---
+++ test/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var assert = require ("assert");
+var argp = require ("../lib");
+
+var tests = {
+	
+};
+
+for (var test in tests){
+	tests[test] ();
+}
