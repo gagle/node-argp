980710680995a13fadffe1499d7e0a4dc7f58146
Gabriel Llamas <gabriel_llamas_llopis@yahoo.es>
2013-09-11 16:45:20 +0200

    added body

--- a.js
+++ a.js
@@ -12,20 +12,22 @@ var argv = argp
 			//allowUndefinedOptions: false
 			//allowUndefinedArguments: false
 		})
-		.option ({ short: "a", argument: "NUM", optional: true })
-		//.text ("some text some text")
-		.option ({ long: "b", argument: "NUM", optional: false, description: "asda sdas asdas asd asd asdasasd asd asd asdas asd adas asdasdas asd asdas asd asd asda asd asdasd asdas asd asd asd asda sd" })
-		.option ({ long: "cc", short: "c", description: "cccccccc" })
-		.group ("Informational options asd asd adas asdqqwe qwe qeqe qwe qe qwe qwe qwe qwe qqwe qwe")
-		//.version ("v0.0.0")
+		.version ("v0.0.0")
+		.email ("a@b.c")
+		.description ("Does something magic that I still don't know asd asdasdasdasdas asd asd asasd asda sassda dasd asd")
+		.usage ("sdasd ad adasd asdas asd asasas asdas asd asd asdas asd asdas asd asdas")
+		.usage ("sdasd ad adasd asdas asd asasas asdas asd asd asdas asd asdas asd asdas asd 123")
 		.argument ("asdetertrqweqwe")
 		.argument ("qwedfgdfgdfg")
 		.argument ("dfgdfgdfg")
-		.description ("Does something magic that I still don't know asd asdasdasdasdas asd asd asasd asda sassda dasd asd")
-		.email ("a@b.c")
-		.usage ("sdasd ad adasd asdas asd asasas asdas asd asd asdas asd asdas asd asdas")
-		.usage ("sdasd ad adasd asdas asd asasas asdas asd asd asdas asd asdas asd asdas asd 123")
-		.version ("v0.0.0")
+		.body (function (body){
+			body
+					.option ({ short: "a", argument: "NUM", optional: true })
+					.text ("some text some text some text some text some text some text some text some text some text some text")
+					.option ({ long: "b", argument: "NUM", optional: false, description: "asda sdas asdas asd asd asdasasd asd asd asdas asd adas asdasdas asd asdas asd asd asda asd asdasd asdas asd asd asd asda sd" })
+					.option ({ long: "cc", short: "c", description: "cccccccc" })
+					.group ("Informational options asd asd adas asdqqwe qwe qeqe qwe qe qwe qwe qwe qwe qqwe qwe")
+		})
 		.argv ();
 
 console.log (argv);
