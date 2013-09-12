abcc363bbdbe959618b4c64a9a56ef37f0f8756d
Gabriel Llamas <gabriel_llamas_llopis@yahoo.es>
2013-09-12 14:37:23 +0200

    fixs when printing --help and --usage

--- o.js
+++ o.js
@@ -1,9 +1,6 @@
 //console.log (require ("optimist").argv);
 //console.log (require ("./lib").argv ())
 
-var opts = require ("./lib").body (function (body){
-	body.option ({ short: "a", argument: "a" });
-				body.option ({ short: "b", argument: "a" });
-}).argv ();
+var opts = require ("./lib").configuration ({ showHelp: true, showUsage: true }).argv ();
 
 console.log (opts);
