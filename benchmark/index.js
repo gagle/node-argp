8bb5770829a786454e263a79b9ac62c241796892
Gabriel Llamas <gabriel_llamas_llopis@yahoo.es>
2013-09-12 02:37:30 +0200

    preparing benchs

---
+++ benchmark/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+process.argv = ["node", __filename, ];
+
+speedy.run ({
+	argp: function (){
+		
+	},
+	commander: function (){
+		
+	},
+	nopt: function (){
+		
+	},
+	optimist: function (){
+		
+	}
+});
