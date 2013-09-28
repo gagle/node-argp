428e199fc1abb45277b593f482cb385d89310810
Gabriel Llamas <gabriel_llamas_llopis@yahoo.es>
2013-09-28 13:50:52 +0200

    improved example

--- examples/modifying-builtin-options.js
+++ examples/modifying-builtin-options.js
@@ -1,13 +1,20 @@
 "use strict";
 
-var argv = require ("../lib")
+var argp = require ("../lib");
+
+argp
 		.body ()
 				.option ({ short: "v", long: "verbose" })
 				.help ()
 				.usage ()
 				//Disable -v option
-				.version ("v1.2.3", { short: false })
-				.argv ();
+				.version ("v1.2.3", { short: false });
+
+argp.options ().help.description = "???";
+
+var argv = argp.argv ();
+//If you cache the module, remember to null it when you finish with it
+argp = null;
 
 console.log (argv);
 
@@ -17,7 +24,7 @@ $ node modifying-builtin-options.js --help
 Usage: modifying-builtin-options [options]
 
   -v, --verbose
-  -h, --help                  Display this help message and exit
+  -h, --help                  ???
       --usage                 Display a short usage message and exit
       --version               Output version information and exit
 */
